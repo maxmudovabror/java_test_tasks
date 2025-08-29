0c6ad33d97891b4a9620d1c4fe9ba82aa2b4be54
Abrorxo‘ja <maxmudovabror7687@gmail.com>
2025-08-29 15:11:01 +0500

    two_sums proyekt

---
+++ two_sums/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 5, 6, 7, 8};
+        int target = 11;
+
+        int left = 0, right = arr.length - 1;
+
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+            if (sum == target) {
+                System.out.println(arr[left] + " + " + arr[right] + " = " + target);
+                return;
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        System.out.println("No pair found.");
+    }
+}
